75a85149fc04176b9420c264291d7c30c2fd3c69
NguyenHoa22643291 <22643291.hoa@student.iuh.edu.vn>
2026-04-13 19:44:38 +0700

    fix

--- web_banVePhim/GatewayService/src/main/java/fit/se/gatewayservice/GatewayServiceApplication.java
+++ web_banVePhim/GatewayService/src/main/java/fit/se/gatewayservice/GatewayServiceApplication.java
@@ -25,7 +25,7 @@ public class GatewayServiceApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://192.168.137.129:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
